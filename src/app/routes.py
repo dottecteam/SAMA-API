01bf9b3d326c4992b01928b9e15a49b2528cb2e4
Cacow69 <cmfcursino@gmail.com>
2025-03-13 09:57:00 -0300

    chore: Adição de páginas e modificações na estilização do template.

--- src/app/routes.py
+++ src/app/routes.py
@@ -3,4 +3,16 @@ from flask import render_template, request, redirect, url_for, jsonify, session
 
 @app.route("/")
 def home():
-    return render_template("template.html")
+    return render_template("vw_home.html")
+
+@app.route("/avaliacao")
+def avaliacao():
+    return render_template("vw_avaliacoes.html")
+
+@app.route("/atestados")
+def atestados():
+    return render_template("vw_atestados.html")
+
+@app.route("/painel")
+def painel():
+    return render_template("vw_painel.html")
