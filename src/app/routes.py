ba2754ba94868eac15b17c27dfd0d22339cddb55
João Paulo <suprejaoee@gmail.com>
2025-03-14 17:38:26 -0300

    feat: Adiciona novas rotas e organizando/estilizando melhor o template.

--- src/app/routes.py
+++ src/app/routes.py
@@ -1,18 +1,42 @@
 from app import app
 from flask import render_template, request, redirect, url_for, jsonify, session
 
+#TELAS
+#Home
 @app.route("/")
 def home():
     return render_template("vw_home.html")
 
-@app.route("/avaliacao")
-def avaliacao():
-    return render_template("vw_avaliacoes.html")
-
-@app.route("/atestados")
+#Página de cadastro de atestados
+@app.route("/atestados/cadastro")
 def atestados():
-    return render_template("vw_atestados.html")
+    return render_template("vw_form_atestados.html")
 
-@app.route("/painel")
-def painel():
-    return render_template("vw_painel.html")
+#Página de cadastro de atestados
+@app.route("/atestados/acesso")
+def acesso_secretaria():
+    return render_template("vw_form_acesso.html")
+
+#Página de painel de atestados
+@app.route("/painel/atestados")
+def painel_atestados():
+    return render_template("vw_dashboard_atestados.html")
+
+#Página de painel de atestados
+@app.route("/painel/equipes")
+def painel_equipes():
+    return render_template("vw_dashboard_equipes.html")
+
+#Página de cadastro de equipes
+@app.route("/equipes/cadastro")
+def equipes():
+    return render_template("vw_form_equipes.html")
+
+@app.route("/equipes/avaliacoes")
+def avaliacao():
+    return render_template("vw_form_avaliacoes.html")
+#TELAS
+
+#FUNÇÕES
+
+#FUNÇÕES
