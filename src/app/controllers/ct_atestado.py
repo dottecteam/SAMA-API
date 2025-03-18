b0c2c57c47af6e6e12dd6415d0b83f4f0eb9c2d6
João Paulo <suprejaoee@gmail.com>
2025-03-18 16:55:14 -0300

    chore: Estrutura do cadastro de usuários para o padrão MVC.

--- src/app/controllers/ct_atestado.py
+++ src/app/controllers/ct_atestado.py
@@ -1,26 +1,45 @@
-#Caminho do arquivo .txt
-caminho_arquivo = "../data/usuarios.txt"  #O caminho "base" do projeto está em /src pois é onde roda o 'run.py'
+from flask import render_template, request, redirect, url_for, jsonify, session
+from app.models.md_atestados import Atestados
+import uuid
 
-#Caminho do arquivo .txt
-caminho_atestados = "../data/atestados"
-
-#Função para salvar os dados no arquivo .txt
-def salvar_dados(nome, email, curso, semestre, dataIn, dataFin, cid, nome_unico):
-    with open(caminho_arquivo, "a") as arquivo:
-        arquivo.write(f"{nome};{email};{curso};{semestre};{dataIn};{dataFin};{cid};{nome_unico}\n")
-
-#Função para ler os dados do arquivo .txt
-def ler_dados():
+def cadastrar_atestado():
     try:
-        with open(caminho_arquivo, "r") as arquivo:
-            linhas = arquivo.readlines()
-            return [linha.strip().split(";") for linha in linhas if linha.strip()]
-    except FileNotFoundError:
-        return []
+        nome = request.form['input-nome-form-atestados']
+        email = request.form['input-email-form-atestados']
+        curso = request.form['input-curso-form-atestados']
+        semestre = request.form['select-form-atestados']
+        dataIn = request.form['date-form-atestados-1']
+        dataFin = request.form['date-form-atestados-2']
+        cid = request.form['input-cid-form-atestados']
+        arquivo = request.files['file-form-atestados']
+
+        if arquivo.filename.endswith(".pdf"):
+            nome_unico = str(uuid.uuid4()) + arquivo.filename
+            caminho_atestados = f"../data/atestados/{nome_unico}"
+            response = Atestados.salvar_arquivo(arquivo, caminho_atestados) 
+            if response != True:
+                return jsonify({"status": False, "mensagem": "Erro ao salvar arquivo!"}), 400
+
+        response = Atestados.salvar_dados(nome, email, curso, semestre, dataIn, dataFin, cid, nome_unico)
+        if response != True:
+            return jsonify({"status": False, "mensagem": "Erro ao salvar dados!"}), 400
+
+        return jsonify({"status": True, "mensagem": "Atestado cadastrado com sucesso!"}), 200 
+    except Exception as e:
+        print(e)
+        return jsonify({"status": False, "mensagem": "Erro ao cadastrar atestado!"}), 400
+        
+        
+
+        
+        
+       
+            
     
-#Função para salvar atestados em .pdf
-def salvar_arquivo(arquivo, caminho_atestados):
-    with open(caminho_atestados, "wb") as f:
-        f.write(arquivo.read())
+         
+
+
+
+
     
  
---
+++ src/app/models/md_atestados.py
@@ -0,0 +1,27 @@
+
+class Atestados:
+    #Caminho do arquivo .txt
+    caminho_arquivo = "../data/usuarios.txt"  #O caminho "base" do projeto está em /src pois é onde roda o 'run.py'
+    #Caminho do arquivo .txt
+    caminho_atestados = "../data/atestados"
+
+    #Função para salvar os dados no arquivo .txt
+    def salvar_dados(nome, email, curso, semestre, dataIn, dataFin, cid, nome_unico):
+        with open(Atestados.caminho_arquivo, "a") as arquivo:
+            arquivo.write(f"{nome};{email};{curso};{semestre};{dataIn};{dataFin};{cid};{nome_unico}\n")
+            return True
+
+    #Função para ler os dados do arquivo .txt
+    def ler_dados():
+        try:
+            with open(Atestados.caminho_arquivo, "r") as arquivo:
+                linhas = arquivo.readlines()
+                return [linha.strip().split(";") for linha in linhas if linha.strip()]
+        except FileNotFoundError:
+            return []
+    
+    #Função para salvar atestados em .pdf
+    def salvar_arquivo(arquivo, caminho_atestados):
+        with open(caminho_atestados, "wb") as f:
+            f.write(arquivo.read())
+            return True
