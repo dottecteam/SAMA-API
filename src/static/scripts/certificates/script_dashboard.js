5c97c975af4d047bd94128f140ae1528f26c16c7
João Paulo <jopaulo.as8@gmail.com>
2025-04-22 18:40:55 -0300

    fix: Arrumando dados do usuário no dashboard

--- src/static/scripts/certificates/script_dashboard.js
+++ src/static/scripts/certificates/script_dashboard.js
@@ -99,7 +99,7 @@ function construirTabelaAtestados(estadoAtestados) {
   for (var i = 0; i < Object.keys(atestadosFiltrados).length; i++) {
     var row =   `<tr>
                   <td>${i+1}°</td>
-                  <td>${ atestadosFiltrados[i][0] }</td>
+                  <td>${ atestadosFiltrados[i][1] }</td>
                   <td>${ atestadosFiltrados[i][2] }</td>
                   <td>${ atestadosFiltrados[i][5] }</td>
                   <td>${ atestadosFiltrados[i][6] }</td>
