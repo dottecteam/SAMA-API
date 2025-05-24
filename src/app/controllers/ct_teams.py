ad58f432912447ff2b23e524d3c99f85e916e2bc
Cacow69 <cmfcursino@gmail.com>
2025-05-23 22:04:37 -0300

    chore: Verificação de integrantes repetidos na equipe

--- src/app/controllers/ct_teams.py
+++ src/app/controllers/ct_teams.py
@@ -4,6 +4,14 @@ from app.models.md_users import Users
 from app.utilities.ut_validation import Validation
 from functools import wraps
 from app.models.md_log import Log
+import shortuuid
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
+from app import app
+from app import admin_email, admin_password
+import os
 
 class TeamsController:
     def loginRequired(f):
@@ -45,14 +53,23 @@ class TeamsController:
             teams=Teams()
 
             # Verifica se os desenvolvedores estão cadastrados
-            if Validation.UserIsRegistered(EmMaster)==False:
+            if Validation.UserIsRegistered(EmMaster) == False:
                 return jsonify({"status": False, "message": f"Desenvolvedor {EmMaster} não cadastrado."}), 400
-            if Validation.UserIsRegistered(EmPOwner)==False:
+            if Validation.UserIsRegistered(EmPOwner) == False:
                 return jsonify({"status": False, "message": f"Desenvolvedor {EmPOwner} não cadastrado."}), 400
+
+            # Verifica se há desenvolvedores duplicados
+            if len(dev_emails) != len(set(dev_emails)):
+                return jsonify({"status": False, "message": "Um mesmo desenvolvedor está sendo adicionado mais de uma vez."}), 400
+
             for email in dev_emails:
-                if Validation.UserIsRegistered(email)==False:
+                if Validation.UserIsRegistered(email) == False:
                     return jsonify({"status": False, "message": f"Desenvolvedor {email} não cadastrado."}), 400
                 
+            # Verifica se o Master ou PO está repetido na lista de devs
+            if EmMaster in dev_emails or EmPOwner in dev_emails:
+                return jsonify({"status": False, "message": "Scrum Master ou Product Owner não pode ser listado como desenvolvedor."}), 400
+                            
             # Busca o nome dos membros pelo email
             users = Users()
             devs = []
@@ -69,7 +86,10 @@ class TeamsController:
             master = users.readUser(EmMaster)[0].name
             pOwner = users.readUser(EmPOwner)[0].name
 
-            if teams.saveDataTeam(team, password, master, EmMaster, pOwner, EmPOwner, devs):
+            id = str(shortuuid.uuid())
+            TeamsController.sendEmail(id,EmMaster)
+
+            if teams.saveDataTeam(id ,team, password, master, EmMaster, pOwner, EmPOwner, devs):
                 Log().register(operation=f'Team: Register Team')
                 return jsonify({"status": True, "message": "Equipe cadastrada com sucesso!"}), 200
             else:
@@ -97,7 +117,6 @@ class TeamsController:
             return jsonify({"status": False, "message": "Erro ao ler dados da equipe!"}), 500
         
     #Função para editar equipe
-    @staticmethod
     def update_team():
         try:
             if 'team' not in session:
@@ -110,6 +129,7 @@ class TeamsController:
             team_name = data.get('teamName')
             EmMaster = data.get('EmMaster')
             EmPOwner = data.get('EmPOwner')
+            
             dev_emails = data.get('dev_email', [])
 
             # Verifica se os desenvolvedores está cadastrado
@@ -121,20 +141,35 @@ class TeamsController:
                 if Validation.UserIsRegistered(email)==False:
                     return jsonify({"status": False, "message": f"Desenvolvedor {email} não cadastrado."}), 400
 
-            # Combina nomes e emails em uma lista de dicionários
-            devs = [{"email": email} for email in zip(dev_emails)]
+            # Busca o nome dos membros pelo email
+            users = Users()
+            devs = []
+            for email in dev_emails:
+                user = users.readUser(email)
+                nome = user[0].name
+                devId = f"{(dev_emails.index(email) + 1):03}"
+                dev = {
+                    'devId': devId,
+                    'email': email,
+                    'nome': nome
+                }
+                devs.append(dev)
+            master = users.readUser(EmMaster)[0].name
+            pOwner = users.readUser(EmPOwner)[0].name
 
             # Chama o método do model para atualizar
-            if Teams().update_team(team_id, team_name, EmMaster, EmPOwner, devs):
+            if Teams().update_team(team_id, team_name, master, pOwner, EmMaster, EmPOwner, devs, img):
                 # Atualiza a sessão
                 session['team'] = {
                     'id': team_id,
                     'team': team_name,
+                    'master': master,
+                    'pOwner': pOwner,
                     'EmMaster': EmMaster,
                     'EmPOwner': EmPOwner,
-                    'devs': devs
+                    'devs': devs,
+                    'img': img
                 }
-                session.modified = True
 
                 Log().register(operation=f'Team: Update Team Data ({team_id})')
                 return jsonify({"status": True, "message": "Equipe atualizada com sucesso!"}), 200
@@ -163,6 +198,8 @@ class TeamsController:
                     devId = dev['devId']
                     evaluations[devEmail][ev] = raw_evaluations[devId + '_' + ev]
 
+            print(evaluations)
+
             if teams.save_evaluations(evaluations):
                 Log().register(operation=f'Team: Evaluation Saved')
                 return jsonify({"status": True, "message": "Avaliações salvas com sucesso!"}), 200
@@ -173,3 +210,77 @@ class TeamsController:
             print(e)
             Log().register(operation=f'Team: Failed Evaluation Attempt')
             return jsonify({"status": False, "message": "Não foi possível salvar avaliações"}), 500
+        
+    def sendEmail(id, email):
+        try:
+            logo_path = os.path.join(app.root_path, "..", "static", "images", "logo.jpg")
+            body = f"""
+            <html>
+                <body style="font-family: 'Arial', sans-serif; background-color: #f4f4f9; color: #333333; margin: 0; padding: 0;">
+                    <!-- Container Principal -->
+                    <div style="width: 100%; max-width: 600px; margin: 0 auto; padding: 20px; background-color: #ffffff; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+                        
+                        <!-- Logo -->
+                        <div style="text-align: center; margin-bottom: 30px;">
+                            <img src="cid:logo" style="border-radius: 15px; width: 200px; margin-bottom: 20px;" alt="Logo" />
+                        </div>
+
+                        <!-- Título -->
+                        <h2 style="text-align: center; font-size: 24px; color: #333333; font-weight: 600;">Cadastro de Equipe</h2>
+
+                        <!-- Mensagem -->
+                        <p style="text-align: center; font-size: 16px; color: #555555; margin-top: 10px;">Sua equipe foi cadastrada com sucesso!</p>
+
+                        <!-- Código de Confirmação -->
+                        <div style="text-align: center; margin: 20px 0;">
+                            <h1 style="color: #3b8c6e; font-size: 36px; font-weight: bold; padding: 20px; background-color: #f0f9f4; border-radius: 8px; display: inline-block; min-width: 120px;">
+                                {id}
+                            </h1>
+                        </div>
+
+                        <!-- Instrução -->
+                        <p style="text-align: center; font-size: 14px; color: #777777;">Para o acesso à equipe, utilize esse ID junto à senha cadastrada.</p>
+
+                        <!-- Rodapé -->
+                        <div style="text-align: center; margin-top: 30px; font-size: 12px; color: #888888;">
+                            <p>&copy; 2025 SAMA. Todos os direitos reservados.</p>
+                        </div>
+                    </div>
+                </body>
+            </html>
+            """
+
+            # Criação da mensagem com partes (HTML + Imagem)
+            msg = MIMEMultipart("related")
+            msg['Subject'] = 'Cadastro de Equipe'
+            msg['From'] = admin_email
+            msg['To'] = email
+
+            # Adicionando o corpo HTML
+            customMsg = MIMEMultipart("alternative")
+            customMsg.attach(MIMEText(body, 'html'))
+            msg.attach(customMsg)
+
+             # Adicionando a imagem da logo
+            try:
+                with open(logo_path, 'rb') as img:
+                    image = MIMEImage(img.read())
+                    image.add_header('Content-ID', '<logo>')
+                    msg.attach(image)
+            except Exception as e:
+                print(f"Error: {e}")
+
+            # Enviando e-mail
+            try:
+                server = smtplib.SMTP('smtp.gmail.com', 587)
+                server.starttls()
+                server.login(admin_email, admin_password)
+                server.sendmail(admin_email, email, msg.as_string())
+                server.quit()
+                return True
+            except Exception as e:
+                print(f"Error: {e}")
+                return False
+        except Exception as e:
+            print(f"Error: {e}")
+            return False
